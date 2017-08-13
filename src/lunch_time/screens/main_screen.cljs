f0b20b2d6aeb8fc31d3854ff590e7dfbc72ce61b
Juho Leinonen <juholei@gmail.com>
2017-08-13 16:06:39 +0300

    Create text and button components to clean the repeating code

--- src/lunch_time/screens/main_screen.cljs
+++ src/lunch_time/screens/main_screen.cljs
@@ -20,30 +20,38 @@
 (defn- lunch-complete? [start-time end-time]
   (and (not= @start-time nil) (not= @end-time nil)))
 
+(def text-style {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}})
+
+(def touchable-style {:background-color "#999" :padding 10 :border-radius 5})
+
+(defn text-element [data string]
+  (when @data
+    [text text-style string (time-format/unparse format (time/to-default-time-zone data))]))
+
+(defn lunch-button [visible? on-press string]
+  (when visible?
+    [touchable-highlight {:style touchable-style
+                          :on-press on-press}
+     [text {:style {:color "white" :text-align "center" :font-weight "bold"}} string]]))
+
 (defn main-screen []
   (let [start-time (subscribe [:get-start-time])
         end-time (subscribe [:get-end-time])]
     (fn []
       [view {:style {:flex-direction "column" :margin 40 :align-items "center"}}
-       (when @start-time
-         [text {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}} "Went to lunch at " (time-format/unparse format (time/to-default-time-zone @start-time))])
-       (when @end-time
-         [text {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}} "Came back from lunch at " (time-format/unparse format (time/to-default-time-zone @end-time))])
-       (when (lunch-complete? start-time end-time)
-         [text {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}} "You were at lunch for " (time/in-minutes (time/interval @start-time @end-time)) " minutes"])
        [image {:source logo-img
                :style  {:width 80 :height 80 :margin-bottom 30}}]
-       (when (= nil @start-time)
-         [touchable-highlight {:style {:background-color "#999" :padding 10 :border-radius 5}
-                               :on-press #(dispatch [:set-start-time (time/now)])}
-          [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "Lunch time!"]])
-       (when (and @start-time (= @end-time nil))
-         [touchable-highlight {:style {:background-color "#999" :padding 10 :border-radius 5}
-                               :on-press #(dispatch [:set-end-time (time/now)])}
-          [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "Back to work!"]])
+       [text-element start-time "Went to lunch at "]
+       [text-element end-time "Came back from lunch at "]
        (when (lunch-complete? start-time end-time)
-         [touchable-highlight {:style {:background-color "#999" :padding 10 :border-radius 5}
-                               :on-press #(do
-                                            (dispatch [:set-end-time nil])
-                                            (dispatch [:set-start-time nil]))}
-          [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "Reset"]])])))
+         [text text-style "You were at lunch for " (time/in-minutes (time/interval @start-time @end-time)) " minutes"])
+       [lunch-button (= nil @start-time)
+                     #(dispatch [:set-start-time (time/now)])
+                     "Lunch time!"]
+       [lunch-button (and @start-time (= @end-time nil))
+                     #(dispatch [:set-end-time (time/now)])
+                     "Back to work!"]
+       [lunch-button (lunch-complete? start-time end-time)
+                     #(do (dispatch [:set-end-time nil])
+                          (dispatch [:set-start-time nil]))
+                     "Reset"]])))
